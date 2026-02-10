8c353c96c8126a3d0960b5675e72bdaa46ffeae6
Deena <deenalinehan@gmail.com>
2026-02-10 14:10:54 -0600

    updates to railway deployment

--- config/connection.js
+++ config/connection.js
@@ -11,7 +11,7 @@ const sequelize = new Sequelize(
   {
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 3306,
-    dialect: process.env.DB_DIALECT || 'mysql',
+    dialect: 'mysql',
     logging: false,
   }
 );
